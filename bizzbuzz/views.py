9c81172f2fa40bca45b22495c3c1edfe600d4720
calloway <calloway@adobe.com>
2020-07-27 10:36:34 -0400

    Added Forbes stuff back, commented out MW stuff

--- bizzbuzz/views.py
+++ bizzbuzz/views.py
@@ -112,8 +112,10 @@ def home_view(request):
                         sources.append('NYTIMES')
                     elif 'techtimes.com' in getattr(n, 'url').lower():
                         sources.append('TECH_TIMES')
-                    elif 'marketwatch.com' in getattr(n, 'url').lower():
-                        sources.append('MARKET_WATCH')
+                    elif 'forbes.com' in getattr(n, 'url').lower():
+                        sources.append('FORBES')
+                    # elif 'marketwatch.com' in getattr(n, 'url').lower():
+                    #     sources.append('MARKET_WATCH')
                     else:
                         sources.append('BI')
         #zip together titles, urls, summaries, sources, and send to home.html
